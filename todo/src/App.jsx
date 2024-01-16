32923cccd0ae9144a8c5f286e58783d0035b833e
Bitterpython <150811900+Bitterpython@users.noreply.github.com>
2024-01-16 14:31:27 +0000

    wip

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,10 +2,45 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([])
 
-  return (<></>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setTodos([...todos, todo])
+  }
+
+  return (<>
+    <form onSubmit={handleSubmit}>
+      <label>Enter your todo:
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)} />
+
+      </label>
+      <input type="submit" />
+    </form>
+
+  
+
+
+
+    <div class="todo-list">
+      {
+        todos.map((todo, index) => {
+          return (<><p key={index}>{todo}</p >
+
+            <button> Löschen </button>
+
+          </>)
+        })
+      }
+    </div>
+  </>
   )
-}
 
+}
 export default App
+
