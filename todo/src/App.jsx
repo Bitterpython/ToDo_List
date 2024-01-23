c7a2bae9b1b0a27cc81c907f4c62fa31e8a28964
Bitterpython <150811900+Bitterpython@users.noreply.github.com>
2024-01-23 14:30:17 +0000

    background grey

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,6 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 
+
+function Delete_Button({ onDeleteFunction }) {
+  return (
+    <button style={{
+      backgroundColor: "red",
+      color: "white",
+
+
+
+    }} onClick={() => {
+      onDeleteFunction()
+    }}>Delete</button>
+  )
+}
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([])
@@ -9,32 +23,66 @@ function App() {
     event.preventDefault();
 
     setTodos([...todos, todo])
+    setTodo("")
   }
+
+
+
+
 
   return (<>
     <form onSubmit={handleSubmit}>
-      <label>Enter your todo:
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        color: "black",
+      }}>
+
+        <p>Eingabe</p>
         <input
           type="text"
           value={todo}
           onChange={(e) => setTodo(e.target.value)} />
 
-      </label>
-      <input type="submit" />
-    </form>
+        <button style={{
+          backgroundColor: "green",
+          color: "white",
 
-  
+        }} onClick={() => {
+          handleSubmit()
+        }}>Add</button>
+      </div>
+
+    </form >
 
 
 
-    <div class="todo-list">
+
+
+    <div class="todo-list" >
       {
         todos.map((todo, index) => {
-          return (<><p key={index}>{todo}</p >
+          return (<div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row",
+            backgroundColor: "lightblue",
+            margin: 10,
+            padding: 10,
+            borderRadius: 10,
+          }}><p key={index}>{todo}</p >
 
-            <button> Löschen </button>
+            <Delete_Button onDeleteFunction={() => {
+              const newTodos = todos.filter((todo, i) => {
+                return i !== index
+              })
+              setTodos(newTodos)
+            }} />
 
-          </>)
+          </div>)
         })
       }
     </div>
